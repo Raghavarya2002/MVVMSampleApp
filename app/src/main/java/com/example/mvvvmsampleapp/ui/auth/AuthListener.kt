b3f77500e5b7a2a8a1a1a4a481815679f67c58b1
Raghav <raghavarya2002@gmail.com>
2021-05-06 22:35:34 +0530

    Learning MVVM architecture

---
+++ app/src/main/java/com/example/mvvvmsampleapp/ui/auth/AuthListener.kt
@@ -0,0 +1,9 @@
+package com.example.mvvvmsampleapp.ui.auth
+
+interface AuthListener {
+
+    fun onStarted()
+    fun onSuccess()
+    fun onFailure(message : String)
+
+}
--- app/src/main/java/com/example/mvvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/ui/auth/LoginActivity.kt
@@ -2,12 +2,36 @@ package com.example.mvvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.example.mvvvmsampleapp.R
+import com.example.mvvvmsampleapp.databinding.ActivityLoginBinding
+import com.example.mvvvmsampleapp.util.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() , AuthListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding :ActivityLoginBinding = DataBindingUtil.setContentView(this , R.layout.activity_login)
+        val viewModel = ViewModelProviders.of(this ).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+    }
+
+    override fun onStarted() {
+        toast("Login Started")
+
+    }
+
+    override fun onSuccess() {
+
+        toast("Login Success")
+
+    }
+
+    override fun onFailure(message: String) {
+
+        toast(message)
 
     }
 }
