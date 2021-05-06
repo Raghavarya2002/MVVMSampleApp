ace28aa7ba1c9eaf62c7c057c0116c1522a2542b
Raghav <raghavarya2002@gmail.com>
2021-05-06 18:51:46 +0530

    Initial Commit

--- app/src/main/java/com/example/mvvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/ui/auth/LoginActivity.kt
@@ -1,13 +1,13 @@
-package com.example.mvvvmsampleapp.ui
+package com.example.mvvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mvvvmsampleapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
     }
 }
