a6cfa8507cca361b6dd3c5ac2ae2531c3c5ab28b
Raghav <raghavarya2002@gmail.com>
2021-05-07 23:23:29 +0530

    Learning MVVM architecture

--- app/src/main/java/com/example/mvvvmsampleapp/data/network/MyApi.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/data/network/MyApi.kt
@@ -4,7 +4,6 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
