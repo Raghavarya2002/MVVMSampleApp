38cfee9e2a93dc9acbcb7b8720631fa347d8414d
Raghav <raghavarya2002@gmail.com>
2021-05-09 16:46:13 +0530

    API is down ,so only learning purpose

--- app/src/main/java/com/example/mvvvmsampleapp/data/network/responses/AuthResponse.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/data/network/responses/AuthResponse.kt
@@ -1,4 +1,10 @@
 package com.example.mvvvmsampleapp.data.network.responses
 
-class AuthResponse {
-}
+import com.example.mvvvmsampleapp.data.db.entities.User
+
+data class AuthResponse(
+    val isSuccessful: Boolean?,
+    val message: String?,
+    val user: User?
+
+)
