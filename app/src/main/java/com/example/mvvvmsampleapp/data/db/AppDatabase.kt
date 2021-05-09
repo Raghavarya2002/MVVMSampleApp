98239598609a3a542d7c591b1a593bbe6cbcc908
Raghav <raghavarya2002@gmail.com>
2021-05-09 16:47:05 +0530

    API is down ,so only learning purpose

--- app/src/main/java/com/example/mvvvmsampleapp/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/data/db/AppDatabase.kt
@@ -1,4 +1,39 @@
 package com.example.mvvvmsampleapp.data.db
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.mvvvmsampleapp.data.db.entities.User
+import java.util.concurrent.locks.Lock
+
+@Database(
+    entities = [User::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun getUserDao(): UserDao
+
+    companion object {
+        @Volatile //this variable is immediately visible to all the other threads
+        private var instance: AppDatabase? = null
+        private val LOCK =
+            Any() //I'll use this lock to make sure we do not create  two instances of our database
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "MyDatabse.db"
+            ).build()
+
+
+    }
 }
