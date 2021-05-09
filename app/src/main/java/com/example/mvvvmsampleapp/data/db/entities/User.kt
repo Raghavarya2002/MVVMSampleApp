200b7659df6204ece305f9d5351cf0e5982296ee
Raghav <raghavarya2002@gmail.com>
2021-05-09 16:48:35 +0530

    API is down ,so only learning purpose

--- app/src/main/java/com/example/mvvvmsampleapp/data/db/entities/User.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/data/db/entities/User.kt
@@ -1,4 +1,22 @@
 package com.example.mvvvmsampleapp.data.db.entities
 
-class User {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+const val CURRENT_USER_ID = 0
+
+@Entity
+data class User(
+    var id: Int? = null,
+    var name: String? = null,
+    var email: String? = null,
+    var password: String? = null,
+    var email_verified_at: String? = null,
+    var created_ar: String? = null,
+    var updated_at: String? = null
+
+
+) {
+    @PrimaryKey(autoGenerate = false)
+    var uid: Int = CURRENT_USER_ID
 }
