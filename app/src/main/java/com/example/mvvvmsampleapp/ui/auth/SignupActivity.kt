5c7f78ab1e05f4a249621ec75a80630ce70c3af0
Raghav <raghavarya2002@gmail.com>
2021-05-10 20:21:16 +0530

    User Signup

--- app/src/main/java/com/example/mvvvmsampleapp/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/example/mvvvmsampleapp/ui/auth/SignupActivity.kt
@@ -1,12 +1,65 @@
 package com.example.mvvvmsampleapp.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.mvvvmsampleapp.R
+import com.example.mvvvmsampleapp.data.db.entities.User
+import com.example.mvvvmsampleapp.databinding.ActivitySignupBinding
+import com.example.mvvvmsampleapp.ui.home.HomeActivity
+import com.example.mvvvmsampleapp.util.hide
+import com.example.mvvvmsampleapp.util.show
+import com.example.mvvvmsampleapp.util.snackbar
+import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class SignupActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: AuthViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
+
+        val binding: ActivitySignupBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_signup)
+        val viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+
+        viewModel.getLoggedInUser().observe(this, Observer { user ->
+            if (user != null) {
+                Intent(this, HomeActivity::class.java).also {
+                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(it)
+                }
+            }
+        })
+
+    }
+
+    override fun onStarted() {
+        progress_bar.show()
+
+    }
+
+    override fun onSuccess(user: User) {
+        progress_bar.hide()
+
+
+    }
+
+
+    override fun onFailure(message: String) {
+        progress_bar.hide()
+        root_layout.snackbar(message)
+
     }
 }
